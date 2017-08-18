667f79526a6374235161fc1a6aab713564919862
Naruth Kongurai <naruth1212@gmail.com>
2017-08-18 17:39:22 +0700

    Multiple revisions made. Fixtures, Standings completed

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
-# Soccer Updates CLI
+# Liverpool F.C. (Soccer) - League, Scores, Updates using Command-Line-Interface
 
-Future project. Coming soon.
+## Overview
+Having been a Liverpool Football Club supporter for the past eight years, I wanted to share with all of you a "nerdy" way of looking up statistics for the Liverpool team, such as league standing, score reports, and players information. 
+
+This command-line-interface application is written in Python, a programming language that I am currently self-studying for.
+
+## Features
+
+You can use the application to look up:
+- List of competition names besides the one that Liverpool F.C. is in (Premier League)
+- Fixtures over a specific number of days that you use as input
+- Live score report
+- League standing
+- Players (Squad)
+- News and updates about the team
+
+## Installation
+
+Clone this repository. Make sure you already have Python installed in your working computer. Then run, `pip install .`
+
+## Usage
+
+Type `cli-soccer -h` to see an overview of available commands.
+
+To retrieve the fixtures for the next 20 days, type `cli-soccer --fixtures='20'`. Simply replace `20` with your number of choice.
+
+To see the current league standing (updated live), type `cli-soccer --standings`. That's it!
+## Report
+
+To report an issue, please send an email to `naruthk@uw.edu`
+
+## Copyright
+
+MIT License. 
--- starter.py
+++ starter.py
@@ -1,49 +1,116 @@
 import click
 import requests
 import json
+from texttable import Texttable, get_color_string, bcolors
 
 base_url = 'http://api.football-data.org/'
 headers = {'X-Auth-Token': 'c2c9d320022e49d184e6845a7e88673d'}
-app_url = 'v1/competitions'
-data = requests.get(base_url+app_url, headers=headers).json()
+
+global table
+table = Texttable()
 
 @click.command(context_settings=dict(max_content_width=200, help_option_names=['-h', '--help']))
 @click.option('--info', is_flag=True, help='Lists competition names')
-@click.option('--fixtures', is_flag=True, help='Looks up a competition (i.e. [PL] Premier League)\'s upcoming fixtures')
+@click.option('--fixtures', help='Looks up Liverpool fixtures by a certain nuimber of days from now')
 @click.option('--live', is_flag=True, help='Live score report')
 @click.option('--standings', is_flag=True, help='Retrieves the a specific competition\'s current standings')
-@click.option('--team', is_flag=True, help='Retrieves information of a particular team')
-@click.option('--about', is_flag=True, help='Show developer\'s details')
+@click.option('--players', is_flag=True, help='Retrieves all players in the Liverpool squad')
+@click.option('--news', is_flag=True, help='Show Liverpool team news')
 
-def start(info, fixtures, live, standings, team, about):
+def start(info, fixtures, live, standings, players, news):
     # Introduction message
-    """Cli-soccer offers a fast, programmatic way of retrieving soccer team news, fixtures, league tables, and live scores. Currently in development stage, and only several teams and leauges will work.
+    """Fetch scores, fixtures, live updates, and news for Liverpool F.C using command-line-interface. 
+
+    "Liverpool Football Club is a professional association football club based in Liverpool, Merseyside, England. They compete in the Premier League, the top tier of English football." - Wikipedia
     
-    For feedback or report, please send an email to naruthk@uw.edu (Naruth Kongurai). Thanks in advance for all the response and support.
+    This CLI application is written in Python as my own personal project. For feedback or report, please send an email to naruthk@uw.edu (Naruth Kongurai).
         
-    
+
     ===================================================
 
     """
 
+    printIntro()
+
     if info:
-        getMasterDetails()
+        getCompetitionDetails()
     if fixtures:
-        getFixtures('PL')
+        getFixtures(fixtures)
     if live:
         click.echo('Currently unsupported')
     if standings:
+        getStandings()
+    if players:
         click.echo('Currently unsupported')
-    if team:
-        click.echo('Currently unsupported')
-    if about:
+    if news:
         click.echo('Currently unsupported')
 
-def getMasterDetails():
+
+def printIntro():
+    print '\nType cli-soccer -h for more help + information.\n'
+
+def getCompetitionDetails():
+    app_url = 'v1/competitions'
+    json_competition = requests.get(base_url+app_url, headers=headers).json()
     print '================= COMPETITIONS ===================='
-    for item in data:
+    for item in json_competition:
         print '    [' + item['league'] + '] - ' + item['caption']
     print '==================================================='
 
-def getFixtures(league):
-    print(league)
+def getFixtures(days):
+    print '========== FIXTURES ===== ' + str(days) + ' days ahead ==========='
+    json_fixtures = ""
+    app_url = 'v1/teams/64/fixtures?timeFrame=n' + str(days)
+    json_fixtures = requests.get(base_url+app_url, headers=headers).json()
+    for item in json_fixtures['fixtures']:
+        # Slice Date from 2017-08-19T14:00:00Z to 19 August 2017 (14:00)
+        datetime = item['date']
+        day = datetime[8:10]
+        month = datetime[5:7]
+        year = datetime[0:4]
+        monthDict = {'01':'January', '02':'February', '03':'March', '04':'April', '05':'May', '06':'June', '07':'July', '08':'August', '09':'September', '10':'October', '11':'November', '12':'December'}
+        print '    [' + day + ' ' + monthDict[month] + ' ' + year + '] - ' + item['status'] + " = " + item['homeTeamName'] + ' vs. ' + item['awayTeamName']
+    print '==================================================='
+
+def getStandings():
+    print '=================== STANDINGS ====================='
+    json_standings = ""
+    app_url = 'v1/competitions/445/leagueTable'
+    json_standings = requests.get(base_url+app_url, headers=headers).json()
+    
+    table.set_cols_align(['c', 'l', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c'])
+    table.set_cols_width(['3', '30', '5', '5', '7', '5', '5', '5', '6', '6'])
+    table.set_deco(Texttable.HEADER)
+    list_pos = []
+    list_teams = []
+    list_wins = []
+    list_draws = []
+    list_losses = []
+    list_goals = []
+    list_goalsagainst = []
+    list_goalsdiff = []
+    list_points = []
+    list_played = []
+
+    for item in json_standings['standing']:
+        list_pos.append(str(item['position']))
+        list_teams.append(item['teamName'])
+        list_wins.append(str(item['wins']))
+        list_draws.append(str(item['draws']))
+        list_losses.append(str(item['losses']))
+        list_goals.append(str(item['goals']))
+        list_goalsagainst.append(str(item['goalsAgainst']))
+        list_goalsdiff.append(str(item['goalDifference']))
+        list_points.append(str(item['points']))
+        list_played.append(str(item['playedGames']))
+
+    table.add_row(['Pos', 'Team', 'Wins', 'Draws', 'Losses', 'Goals', 'GA', 'GD', 'Points', 'Played'])
+    for row in zip(list_pos, list_teams, list_wins, list_draws, list_losses, list_goals, list_goalsagainst, list_goalsdiff, list_points, list_played):
+        table.add_row(row)
+
+    final_standings = table.draw()
+
+    print 'Matchday: ' + str(json_standings['matchday']) + "\n"
+    print final_standings
+
+    print '==================================================='
